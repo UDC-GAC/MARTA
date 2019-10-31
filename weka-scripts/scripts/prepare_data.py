9c95f4594eb0b39454c7fd2f5025ae02657056a4
Marcos Horro <marcos.horro@udc.es>
2019-10-31 13:45:58 -0600

    Created wrapper for better executing experiments. Error checking avoiding ilegible terminal

--- weka-scripts/scripts/prepare_data.py
+++ weka-scripts/scripts/prepare_data.py
@@ -1,4 +1,10 @@
-################################################################################
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : prepare_data.py
+# Author            : Marcos Horro <marcos.horro@udc.gal>
+# Date              : Mar 29 Out 2019 09:38:20 MDT
+# Last Modified Date: Xov 31 Out 2019 13:43:20 MDT
+# Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
 # -*- coding: utf-8 -*-
 # prepare_data.py
 # Copyright (c) 2019 Marcos Horro <marcos.horro@udc.gal>
@@ -21,114 +27,229 @@ import sys
 import argparse
 import pandas as pd
 import numpy as np
+from math import ceil
 
 # global definitions
-PATH_WEKA="~/Descargas/weka-3-8-3"
-PATH_DATA="."
-NSETS=5
-FORCE_REPLACEMENT=False
+PATH_ROOT = os.getcwd() + "/"
+PATH_WEKA = "~/Descargas/weka-3-8-3"
+PATH_DATA = PATH_ROOT + "data"
+PATH_RES = PATH_ROOT + "results"
+NSETS = 0
+FORCE_REPLACEMENT = True
+ALGORITHMS = ['REPTree']
+MIN_OBJ = [1]  # min number of instances
+MAX_LEAF = [3]  # number of folds
+
 
 class StoreDictKeyPair(argparse.Action):
-     def __init__(self, option_strings, dest, nargs=None, **kwargs):
-         self._nargs = nargs
-         super(StoreDictKeyPair, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
-     def __call__(self, parser, namespace, values, option_string=None):
-         my_dict = {}
-         for kv in values:
-             k,v = kv.split("=")
-             my_dict[k] = int(v)
-         setattr(namespace, self.dest, my_dict)
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        self._nargs = nargs
+        super(StoreDictKeyPair, self).__init__(
+            option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        my_dict = {}
+        for kv in values:
+            k, v = kv.split("=")
+            my_dict[k] = int(v)
+        setattr(namespace, self.dest, my_dict)
 
 ##################################################
 # preprocessing data
-def preprocess_data(inputfile, outputfile, cols, *rows, cat="FLOPSs", ncat=20):
-  df = pd.read_csv(inputfile, comment="#", index_col=False)
-  tmp = df[list(cols)] # selecting only those columns
-  for d in rows:
-    if d==None: continue
-    for k,v in d.items():
-      cond = (getattr(tmp,k)==v)
-      tmp = tmp[cond]
-  # categorical data
-  tmp_cat = getattr(tmp,cat)
-  bins = np.linspace(min(tmp_cat),max(tmp_cat),ncat+1)
-  step = bins[1] - bins[0]
-  labels = ["I-{0}-{1}".format("{0:.3f}".format(float(i/1e9)), "{0:.3f}".format(float((i + step)/1e9))) for i in bins]
-  setattr(tmp,cat,pd.cut(tmp_cat, bins, labels=labels[:-1]))
-  tmp.to_csv(outputfile, index=False)
-  return
+
+
+def check_error(err_log):
+    for l in open(err_log, 'r'):
+        if "Exception" in l:
+            print("[ERROR] something went wrong:\n%s" % l)
+            exit(-1)
+
+
+def preprocess_data(inputfile, outputfile, cols, *rows, cat, ncat):
+    df = pd.read_csv(inputfile, comment="#", index_col=False)
+    tmp = df
+    for d in rows:
+        if d == None:
+            continue
+        for k, v in d.items():
+            cond = (getattr(tmp, k) == v)
+            tmp = tmp[cond]
+    if (tmp.count()[0] == 0):
+        print("[ERROR] DATASET EMPTY! Revise constraints in data please!")
+        exit(-1)
+    # categorical data
+    tmp_cat = getattr(tmp, cat)
+    bins = np.linspace(min(getattr(df, cat)), max(getattr(df, cat)), ncat+1)
+    # labels = range(0, ncat+1)
+    step = bins[1] - bins[0]
+    labels = ["I-{0}-{1}".format("{0:.3f}".format(float(i/1e6)),
+                                 "{0:.3f}".format(float((i + step)/1e6))) for i in bins]
+    setattr(tmp, cat, pd.cut(tmp_cat, bins, labels=labels[:-1]))
+    new_cols = []
+    tmp['overhead'] = df.Is*df.Is*df.Is/(df.Js*df.Js)
+    tmp['cl'] = 0.125*(df.It*df.Is) + 0.25 * (df.Js*df.Jt) * df.It/df.Is
+    tmp['loadstore'] = 0
+    # new_cols += ['overhead']
+    # new_cols += ['cl']
+    f_cols = new_cols + cols
+    tmp = tmp[list(f_cols)]
+    tmp.to_csv(outputfile, index=False)
+    return
 
 ##################################################
 # creating files for training and testing
-def train_and_test(datafile):
-  name = datafile.split(".")[0] 
-  pref="_full_rand"
-  for i in range(1,NSETS+1):
-    # training
-    suff="_train.arff"
-    filename="%s_%s%s%s" % (name,str(i),pref,suff)
-    if FORCE_REPLACEMENT or not (os.path.exists("%s/%s" % (PATH_DATA,filename))):
-      os.system("java -cp %s/weka.jar weka.filters.supervised.instance.StratifiedRemoveFolds "\
-                " -i %s/%s -c last -o %s/%s "\
-                " -N %s -F %s -V -S 42 2> %s" % (PATH_WEKA,PATH_DATA,datafile,PATH_DATA,filename,str(NSETS),str(i),err_output))
-    
-    # testing
-    suff="_test.arff"
-    filename="%s_%s%s%s" % (name,str(i),pref,suff)
-    if FORCE_REPLACEMENT or not (os.path.exists("%s/%s" % (PATH_DATA,filename))):
-      os.system("java -cp %s/weka.jar weka.filters.supervised.instance.StratifiedRemoveFolds "\
-                " -i %s/%s -c last -o %s/%s "\
-                " -N %s -F %s -S 42 2> %s" % (PATH_WEKA,PATH_DATA,datafile,PATH_DATA,filename,str(NSETS),str(i),err_output))
-    suff="_all_train.arff"
-    filename="%s_%s%s%s" % (name,str(i),pref,suff)
-    os.system("cp %s/%s %s/%s" % (PATH_DATA,datafile,PATH_DATA,filename))
-    
-    suff="_all_test.arff"
-    filename="%s_%s%s%s" % (name,str(i),pref,suff)
-    os.system("cp %s/%s %s/%s" % (PATH_DATA,datafile,PATH_DATA,filename))
+
+
+def create_train_and_test(datafile):
+    os.system("mkdir -p %s" % PATH_DATA)
+    name = datafile.split(".")[0]
+    pref = "_full_rand"
+    for i in range(1, NSETS+1):
+        # training
+        suff = "_train.arff"
+        filename = "%s_%s%s%s" % (name, str(i), pref, suff)
+        if FORCE_REPLACEMENT or not (os.path.exists("%s/%s" % (PATH_DATA, filename))):
+            os.system("java -cp %s/weka.jar weka.filters.supervised.instance.StratifiedRemoveFolds "
+                      " -i %s -c last -o %s/%s "
+                      " -N %s -F %s -V -S 42 %s" % (PATH_WEKA, datafile,
+                                                    PATH_DATA, filename,
+                                                    str(NSETS), str(i),
+                                                    ERR_OUT))
+        # testing
+        suff = "_test.arff"
+        filename = "%s_%s%s%s" % (name, str(i), pref, suff)
+        if FORCE_REPLACEMENT or not (os.path.exists("%s/%s" % (PATH_DATA, filename))):
+            os.system("java -cp %s/weka.jar weka.filters.supervised.instance.StratifiedRemoveFolds "
+                      " -i %s -c last -o %s/%s "
+                      " -N %s -F %s -S 42 %s" % (PATH_WEKA, datafile,
+                                                 PATH_DATA, filename,
+                                                 str(NSETS), str(i), ERR_OUT))
+    suff = "_train.arff"
+    filename = "%s_all%s%s" % (name, pref, suff)
+    os.system("cp %s %s/%s" % (datafile, PATH_DATA, filename))
+    suff = "_test.arff"
+    filename = "%s_all%s%s" % (name, pref, suff)
+    os.system("cp %s %s/%s" % (datafile, PATH_DATA, filename))
+
+##################################################
+# runing experiments
+
+
+def run_experiments(datafile):
+    os.system("mkdir -p %s" % (PATH_RES))
+    name = datafile.split(".")[0]
+    pref = "_full_rand"
+    liters = list(range(1, NSETS+1))
+    liters.append("all")
+    for alg in ALGORITHMS:
+        for min_obj in MIN_OBJ:
+            for max_leaf in MAX_LEAF:
+                for i in liters:
+                    basename = "%s_%s" % (name, str(i))
+                    foldername = "exp_%s_%s_%s_%s" % (basename,
+                                                      str(alg), str(min_obj), str(max_leaf))
+                    os.system("mkdir -p %s/%s" % (PATH_RES, foldername))
+                    folderpath = PATH_RES + "/" + foldername
+                    trainfile = PATH_DATA + "/" + basename + pref + "_train.arff"
+                    testfile = PATH_DATA + "/" + basename + pref + "_test.arff"
+                    print("\t[%s,%d,%d] Train... (%s/%d)" %
+                          (alg, min_obj, max_leaf, str(i), NSETS))
+                    os.system("java -cp %s/weka.jar"
+                              " weka.classifiers.trees.%s"
+                              " -M %d -N %d -t %s -d %s/output.model"
+                              "  > %s/model_learn_stats.txt %s"
+                              % (PATH_WEKA, alg, min_obj, max_leaf, trainfile,
+                                 folderpath, folderpath, ERR_OUT))
+                    check_error(ERR_FILE)
+                    print("\t[%s,%d,%d] Produce classfied outputs... (%s/%d)" %
+                          (alg, min_obj, max_leaf, str(i), NSETS))
+                    os.system("java -cp %s/weka.jar"
+                              " weka.classifiers.trees.%s"
+                              " -l %s/output.model -T %s -p 0"
+                              "  > %s/pred.txt %s"
+                              % (PATH_WEKA, alg, folderpath, testfile,
+                                 folderpath, ERR_OUT))
+                    print("\t[%s,%d,%d] Produce tester stats... (%s/%d)" %
+                          (alg, min_obj, max_leaf, str(i), NSETS))
+                    os.system("java -cp %s/weka.jar"
+                              " weka.classifiers.trees.%s"
+                              " -l %s/output.model -T %s"
+                              "  > %s/model_test_stats.csv %s"
+                              % (PATH_WEKA, alg, folderpath, testfile,
+                                 folderpath, ERR_OUT))
+    return
+
 
 ##################################################
 # parsing arguments
-parser = argparse.ArgumentParser(description='Convert from CSV to ARFF (generic format), creates training and testing sets. Allows the inclusion of custom scripts (no need to be python) for preparing data according to experiments need. TODO: options for selecting categorical value. NOTE: Need to have WEKA installed.')
-parser.add_argument('-rm', '--rmtemp', action='store_true', help='clean directory of temp files (default=False)', default=False)
-parser.add_argument('-f', '--force', action='store_true', help='force replacement files even if they exist (default=False)', default=False)
-parser.add_argument('-r','--rows', dest="rows", action=StoreDictKeyPair, help="filtering rows as a dictionary. E.g. -r I=0 J=0", nargs="+", metavar="KEY=VAL")
+parser = argparse.ArgumentParser(
+    description='Convert from CSV to ARFF (generic format), creates training and testing sets. Allows the inclusion of custom scripts (no need to be python) for preparing data according to experiments need. TODO: options for selecting categorical value. NOTE: Need to have WEKA installed.')
+parser.add_argument('-p', '--pred',  # action='store_const',
+                    help='dimension to predict', default="FLOPSs")
+parser.add_argument('-nc', '--ncats',  # action='store_const',
+                    help='number of categories of dimension to predict,'
+                    ' categories are created uniformly in pandas',
+                    default=10)
+parser.add_argument('-ml', '--minleaf',  # action='store_const',
+                    help='number of folds', default=1)
+parser.add_argument('-nf', '--nfolds',  # action='store_cont',
+                    help='number of folds', default=3)
+parser.add_argument('-rm', '--rmtemp', action='store_true',
+                    help='clean directory of temp files (default=False)', default=False)
+parser.add_argument('-f', '--force', action='store_true',
+                    help='force replacement files even if they exist (default=False)', default=False)
+parser.add_argument('-r', '--rows', dest="rows", action=StoreDictKeyPair,
+                    help="filtering rows as a dictionary. E.g. -r I=0 J=0", nargs="+", metavar="KEY=VAL")
 requiredNamed = parser.add_argument_group('required named arguments')
-requiredNamed.add_argument('-i', '--input', help='input file name', required=True)
-requiredNamed.add_argument('-o', '--output', help='output file name', required=True)
+requiredNamed.add_argument(
+    '-i', '--input', help='input file name', required=True)
+requiredNamed.add_argument(
+    '-o', '--output', help='output file name', required=True)
 requiredNamed.add_argument('-c', '--columns', metavar='col', type=str, nargs='+',
-                    help='columns we are interested to filter')
+                           help='columns we are interested to filter')
 args = parser.parse_args()
 
-FORCE_REPLACEMENT=args.force
+FORCE_REPLACEMENT = args.force
 INPUT_FILE = args.input
 OUTPUT_FILE = args.output
 COLUMNS = args.columns
-ROWS=args.rows
-err_output="/dev/null"
+ROWS = args.rows
+PRED = args.pred
+NCATS = int(args.ncats)
+MIN_OBJ = [int(i) for i in [args.minleaf]]
+MAX_LEAF = [int(i) for i in [args.nfolds]]
+
+# ERR_OUT = " 2> /dev/null"
+ERR_FILE = "error.log"
+ERR_OUT = " 2> %s" % ERR_FILE
 ##################################################
 
 ##################################################
 # Script based on Pouchet's, but unfollowing him's, I perform the 'filtering' or whatever in the CSV instead of the ARFF
 # Main reason: familiarity and ease to work with DataFrames in python and mantainability for me...
-# 1) Create temporal CSV with filtering perform in terms of columns, rows and even some transformations
-TMP_CSV = "___tmp_" + str(INPUT_FILE)
-print("filtering csv... (", INPUT_FILE, ")")
-preprocess_data(INPUT_FILE, TMP_CSV, COLUMNS, ROWS)
+# 1) create temporal CSV with filtering perform in terms of columns, rows and even some transformations
+TMP_CSV = "___tmp_" + str(INPUT_FILE.split("/")[-1])
+print("[step 1] filtering csv... (", INPUT_FILE, ")")
+preprocess_data(INPUT_FILE, TMP_CSV, COLUMNS, ROWS, ncat=NCATS, cat=PRED)
 
 ##################################################
-# 2) Convert CSV to ARFF file (all data, beware to have unique name for columns in the CSV file):
-print("converting csv to arff format...")
-os.system("java -cp %s/weka.jar weka.core.converters.CSVLoader %s -B 100000 > %s" % (PATH_WEKA,TMP_CSV,OUTPUT_FILE))
+# 2) convert CSV to ARFF file (all data, beware to have unique name for columns in the CSV file):
+print("[step 2] converting csv to arff format...")
+os.system("java -cp %s/weka.jar weka.core.converters.CSVLoader %s -B 100000 > %s" %
+          (PATH_WEKA, TMP_CSV, OUTPUT_FILE))
 
 ##################################################
-# 3) Prepare data: create training and testing sets
-print("creating training and testing sets...")
-train_and_test(OUTPUT_FILE)
+# 3) prepare data: create training and testing sets
+print("[step 3] creating training and testing sets...")
+create_train_and_test(OUTPUT_FILE)
+
+##################################################
+# 4) run experiments
+print("[step 4] running experiments...")
+run_experiments(OUTPUT_FILE)
 
 print("you are all set!")
 ##################################################
-# 4) Clean working directory
+# Clean working directory
 if args.rmtemp:
-  print("[rm option enabled] cleaning temp files!")
-  os.system("rm %s" % TMP_CSV)
+    print("[rm option enabled] cleaning temp files!")
---
+++ weka-scripts/scripts/wrapper.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : wrapper.py
+# Author            : Marcos Horro <marcos.horro@udc.gal>
+# Date              : Xov 31 Out 2019 09:56:07 MDT
+# Last Modified Date: Xov 31 Out 2019 13:42:27 MDT
+# Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
+
+import os
+import itertools as it
+import argparse
+
+# disgusting wrapper
+
+
+class StoreDictKeyPair(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        self._nargs = nargs
+        super(StoreDictKeyPair, self).__init__(
+            option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        my_dict = {}
+        for kv in values:
+            k, v = kv.split("=")
+            my_dict[k] = int(v)
+        setattr(namespace, self.dest, my_dict)
+
+
+stride = range(1, 2)
+cols = 'I J It Jt Iters Jters '
+nfolds = 3
+minleaf = 1
+
+parser = argparse.ArgumentParser(
+    description='Wrapper for prepare data. Quite ugly tho...')
+parser.add_argument('-r', '--rows', dest="rows", action=StoreDictKeyPair,
+                    help="filtering rows as a dictionary. E.g. -r I=0 J=0",
+                    nargs="+", metavar="KEY=VAL")
+parser.add_argument('-p', '--pred',  # action='store_const',
+                    help='dimension to predict', default="FLOPSs")
+parser.add_argument('-nc', '--ncats',  # action='store_const',
+                    help='number of categories of dimension to predict',
+                    default=10)
+args = parser.parse_args()
+INPUT_FILE = "../data/CSV/exec_reg_model.csv"
+OUTPUT_FILE = "testing.arff"
+ROWS = args.rows
+PRED = args.pred
+NCATS = int(args.ncats)
+cols += str(PRED)
+filter_rows = ""
+if ROWS != None:
+    for d in ROWS:
+        filter_rows += "%s=%s " % (str(d), str(ROWS[d]))
+
+summary_file = "summary_file.txt"
+os.system("echo \"\" > %s" % (summary_file))
+for Is, Js in it.product(stride, stride):
+    print("[wrapper] executing for strides Is %2d Js %2d" % (Is, Js))
+    ret = os.system("python3 prepare_data.py -i %s -o %s -r Is=%s Js=%s"
+                    " %s -c %s --nfolds=%s --minleaf=%s --pred=%s --ncats=%s" %
+                    (INPUT_FILE, OUTPUT_FILE, str(Is), str(Js), filter_rows, cols, nfolds,
+                     minleaf, PRED, str(NCATS)))
+    if (ret != 0):
+        print("[wrapper] Something went wrong!")
+    result_file = "results/model_learn_stats_%s_is%s_js%s_folds%s_leaf%s" % (
+        str(PRED), str(Is), str(Js), str(nfolds), str(minleaf))
+    os.system("cp results/exp_%s_all_REPTree_%s_%s/model_learn_stats.txt"
+              " %s" %
+              (OUTPUT_FILE.split(".")[0], minleaf, nfolds, result_file))
+    os.system("grep -m 1 -n \"Correctly\" %s /dev/null >> %s" %
+              (result_file, summary_file))
+
+os.system("cat %s" % summary_file)
