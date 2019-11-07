1895b4127da0b07354beb48be942d1239d3a5753
Marcos Horro <marcos.horro@udc.es>
2019-11-06 17:30:27 -0700

    Now wrapper uses YAML for executing; no need to CLI

--- weka-scripts/scripts/wrapper.py
+++ weka-scripts/scripts/wrapper.py
@@ -3,9 +3,9 @@
 # File              : wrapper.py
 # Author            : Marcos Horro <marcos.horro@udc.gal>
 # Date              : Xov 31 Out 2019 09:56:07 MDT
-# Last Modified Date: Mar 05 Nov 2019 10:59:38 MST
+# Last Modified Date: Mér 06 Nov 2019 17:28:24 MST
 # Last Modified By  : Marcos Horro <marcos.horro@udc.gal>
-# wrapper.py
+#
 # Copyright (c) 2019 Computer Architecture Group, Universidade da Coruña
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -28,94 +28,94 @@
 #
 # Authors: Marcos Horro
 
+import yaml
 import os
 import itertools as it
 import argparse
 from utils.utilities import StoreDictKeyPair
 from utils.utilities import prRed
 from utils.utilities import prGreen
+from utils.utilities import grep_file2file
+from parse_tree import REPTree
 
 ##################################################
 # parsing arguments
 parser = argparse.ArgumentParser(
-    description='Wrapper for prepare data. Quite ugly tho...')
-parser.add_argument('-r', '--rows', dest="rows", action=StoreDictKeyPair,
-                    help="filtering rows as a dictionary. E.g. -r I=0 J=0",
-                    nargs="+", metavar="KEY=VAL")
-parser.add_argument('-p', '--pred',  # action='store_const',
-                    help='dimension to predict', default="FLOPSs")
-parser.add_argument('-nc', '--ncats',  # action='store_const',
-                    help='number of categories of dimension to predict',
-                    default=10)
-parser.add_argument('-ml', '--minleaf',  # action='store_const',
-                    help='number of folds', default=1)
-parser.add_argument('-nf', '--nfolds',  # action='store_cont',
-                    help='number of folds', default=3)
-parser.add_argument('-nv', '--norm', action='store_true',
-                    help='normalize predicted dimension (i.e. [0-1])',
-                    default=False)
-parser.add_argument('-c', '--columns', metavar='col', type=str, nargs='+',
-                    help='columns we are interested to filter')
+    description='Wrapper for preparing data given a CSV')
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument(
+    '-i', '--input', help='input file name', required=True)
 args = parser.parse_args()
 
 ##################################################
-# parsing all the arguments
-INPUT_FILE = "exec_asm_full_filtered.csv"
-OUTPUT_FILE = "testing.arff"
-NFOLDS = args.nfolds
-MINLEAF = args.minleaf
-COLS = args.columns
-NORM = ""
-if args.norm:
-    NORM = "--norm"
-ROWS = args.rows
-PRED = args.pred
-NCATS = int(args.ncats)
-COLS += [PRED]
-FILTER_ROWS = ""
-if ROWS != None:
-    FILTER_ROWS = "-r "
-    for d in ROWS:
-        FILTER_ROWS += "%s=%s " % (str(d), str(ROWS[d]))
-FILTER_COLS = ""
-for c in COLS:
-    FILTER_COLS = FILTER_COLS + " " + c
+# parsing all the arguments from the config.yml
+YML_CONFIG = args.input
+with open(YML_CONFIG, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+# files
+files_cfg = cfg[0]['files']
+INPUT_FILE = files_cfg['input']
+OUTPUT_FILE = files_cfg['output']
+FORCE_REPLACEMENT = files_cfg['overwrite']
+RM_TEMP_FILES = files_cfg['clean']
+# prepare data
+prepdata_cfg = cfg[1]['prepare_data']
+FILTER_COLS = prepdata_cfg['cols']
+FILTER_ROWS = prepdata_cfg['rows']
+PRED = prepdata_cfg['pred']
+MINLEAF = prepdata_cfg['dt_params']['minleaf']
+NFOLDS = prepdata_cfg['dt_params']['nfolds']
+NORM = "--norm" if prepdata_cfg['norm'] else ""
+NCATS = prepdata_cfg['ncats']
 
 SUMM_FILE = "___tmp__SUMM_FILE.txt"
-os.system("echo \"\" > %s" % (SUMM_FILE))
+os.system("echo \"Summary of results: \" > %s" % (SUMM_FILE))
 
 ##################################################
 # executing experiments
 prGreen("[wrapper] executing wrapper...")
 ret = os.system("python3 prepare_data.py -i %s -o %s"
-                " %s -c %s --nfolds=%s --minleaf=%s --pred=%s --ncats=%s"
+                " -r %s -c %s --nfolds=%s --minleaf=%s --pred=%s --ncats=%s"
                 " %s --rmtemp" %
                 (INPUT_FILE, OUTPUT_FILE, FILTER_ROWS,
                  FILTER_COLS, NFOLDS,
                  MINLEAF, PRED, str(NCATS), NORM))
 if (ret != 0):
     prRed("[wrapper] Something went wrong!")
-result_file = "results/model_learn_stats_%s_folds%s_leaf%s" % (
+    exit(1)
+RES_FILE = "results/model_learn_stats_%s_folds%s_leaf%s" % (
     str(PRED), str(NFOLDS), str(MINLEAF))
 
 ##################################################
 # parsing results
 os.system("cp results/exp_%s_all_REPTree_%s_%s/model_learn_stats.txt"
           " %s" %
-          (OUTPUT_FILE.split(".")[0], MINLEAF, NFOLDS, result_file))
-os.system("grep -m 1 -n \"Correctly\" %s /dev/null >> %s" %
-          (result_file, SUMM_FILE))
-os.system("grep -m 1 -n \"Correlation coefficient\" %s /dev/null >> %s" %
-          (result_file, SUMM_FILE))
-os.system("grep -m 1 -n \"Size of the tree\" %s /dev/null >> %s" %
-          (result_file, SUMM_FILE))
+          (OUTPUT_FILE.split(".")[0], MINLEAF, NFOLDS, RES_FILE))
+grep_file2file("Correctly", RES_FILE, SUMM_FILE)
+grep_file2file("Correlation coefficient", RES_FILE, SUMM_FILE)
+grep_file2file("Mean absolute error", RES_FILE, SUMM_FILE)
+grep_file2file("Relative absolute error", RES_FILE, SUMM_FILE)
+grep_file2file("Total Number of Instances", RES_FILE, SUMM_FILE)
+grep_file2file("Size of the tree", RES_FILE, SUMM_FILE)
 
 ##################################################
 # results obtained
-prGreen("[wrapper] results")
+prGreen("[wrapper] displaying summary of results")
 os.system("cat %s" % SUMM_FILE)
 prGreen("[wrapper] finished! cleaning temp files...")
 
 ##################################################
 # remove tmp files
 os.system("rm %s" % SUMM_FILE)
+
+##################################################
+# copy results for parsing tree
+PARSING_TREE_FILE = "___tmp_tree.txt"
+os.system("cp %s %s" % (RES_FILE, PARSING_TREE_FILE))
+
+# testing parsing tree
+tree = REPTree(PARSING_TREE_FILE)
+tree.print_parents_compressed_by_value("I-5.419-6.035")
+
+os.system("rm %s" % PARSING_TREE_FILE)
