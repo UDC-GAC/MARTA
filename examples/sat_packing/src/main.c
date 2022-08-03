0d2b303bd9e5a437b7634719d4a72a1831a5b807
Gabriel Rodríguez <gabriel.rodriguez@udc.es>
2022-08-03 18:01:54 +0200

    Fixed minor regression.

--- examples/sat_packing/src/main.c
+++ examples/sat_packing/src/main.c
@@ -50,7 +50,7 @@ POLYBENCH_1D_ARRAY_DECL(p, DATA_TYPE, N, n);
 // Initialization
 init_1darray(n, POLYBENCH_ARRAY(p));
 
-PROFILE_FUNCTION_LOOP(kernel(POLYBENCH_ARRAY(p) + __marta_tsteps * OFFSET));
+PROFILE_FUNCTION_LOOP(kernel(POLYBENCH_ARRAY(p) + __marta_tsteps * N_OFFSET));
 
 // Free memory, avoid DCE
 // MARTA_AVOID_DCE(POLYBENCH_ARRAY(p));
